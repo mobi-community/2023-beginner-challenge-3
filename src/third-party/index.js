f2998ea21d1016d11356b7b323d425c438218f91
hyunseoKoo <koohs414@naver.com>
2023-07-26 14:39:12 +0900

    :recycle: refactor: pagination 재사용 컴포넌트 생성

---
+++ src/third-party/index.js
@@ -0,0 +1,2 @@
+export * as weather from "./weather.config.js";
+ 
---
+++ src/third-party/weather.config.js
@@ -0,0 +1,6 @@
+export const weatherConfig = (() => {
+  return {
+    api: "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0",
+    secret_key: "BeVmXtoDIrQarY9fKeYXz9vVfTKT1d4SkfQ3KnpnJ2IYOC5T0vTeSkqNvzo57%2FBvv8%2F64b0%2FJpNHASa4vXAMzQ%3D%3D",
+  };
+})();
